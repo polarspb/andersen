bc9249a9edfd15091e69e80083d099fe81aef747
iPolishchuk <79876555+polarspb@users.noreply.github.com>
2021-03-06 09:41:20 -0500

    edit script

--- lesson1/task2/volatility_script.sh
+++ lesson1/task2/volatility_script.sh
@@ -2,6 +2,7 @@
 
 sudo apt install curl -y
 sudo apt install jq -y
+clear
 
 curl -s https://yandex.ru/news/quotes/graph_2000.json > ./quotes.json
 jq -r '.prices[][]' quotes.json | cut -c 1-10 > out1
@@ -19,10 +20,14 @@ done < out1 > out2
 
 for y in 2015 2016 2017 2018 2019 2020
  do
-   k=$(cat out2 | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' -e 's/ 2/\n2/g' | grep -a "$y-03-" | awk 'BEGIN { coun=0; sum=0 } { coun++; sum+=$2 } END { print sum/coun }')
-   echo "$k - $y"
- done > out3
+   cat out2 | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' -e 's/ 2/\n2/g' | grep -a "$y-03" | sort -n -k2 | awk '{ print $1 " " $2 }' > out3
+   var_min=$(cut -d" " -f2 out3 | sed -n '1s/^//p')
+   var_max=$(cut -d" " -f2 out3 | sed -n '$s/^//p')   
+   var_diff=$(awk "BEGIN { print $var_max - $var_min }")
+   echo "$y - $var_diff"
+ done >> out4
 
-cut -d" " -f1,2,3 out3 | sort -n | sed -n '1s/^/min=/p; $s/^/max=/p'
-
-rm out1 out2 out3 quotes.json
+echo
+echo "Least volatile was in March" $(cut -d" " -f1,2,3 out4 | sort -n -k3 | sed -n '1s/^//p')
+echo
+rm out1 out2 out3 out4 quotes.json
