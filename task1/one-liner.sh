15a68e9684dc92d55ed0a70e6868dee093c94f30
polarspb <polarspb@debian-main.domain>
2021-03-08 14:21:13 -0500

    edit script task1

--- task1/one-liner.sh
+++ task1/one-liner.sh
@@ -9,23 +9,25 @@ PINFO=$(netstat -tunapl | awk '{print "$STR" $5 $7}' | cut -d: -f1 | sort | uniq
 PNAME=$(ss -tup | sed -n '/'\"$STR'/p')
 PID=$(ss -tup | sed -n '/'=$STR'/p')
 
-echo "----------------------------------------------"
+echo "-----------------------------------------------"
 echo
 if [ -z "$STR" ]
 then
  echo "Error. Please input process name/ID."
- echo
 elif [[ -z "$PNAME" && -z "$PID" ]]
 then
  echo "Error. Wrong process name/ID."
- echo
 elif [ -n "$PINFO" ] 
 then
  echo "$PINFO" | while read IP ; do whois $IP | awk -F':' '/^Organization/ || /^Address/ || /^City/ || /^Country/ {print $0}' ; done 
- echo 
+ echo
+ echo "----------------------------------------------"
+ echo
+ echo "$PINFO" | while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done | sort | uniq -c | while read COUNT ; do echo "Connections: $COUNT" ; done
 else 
  echo "Error. No connection!"
  echo
 fi
-echo "----------------------------------------------"
 echo
+echo "-----------------------------------------------"
+
