b2a2684531f01cfdb30c31d47722963b8c66c7af
polarspb <polarspb@debian-main.domain>
2021-03-10 09:39:47 -0500

    Edit pull request script

--- task3/README.md
+++ task3/README.md
@@ -1,4 +1,14 @@
-### Practice of education by Andersen Course.
+### Pull Request script
 
-https://andersenlab.com/
+### Script that checks if there are open pull requests for a repository
 
+```
+Examle
+input user name: polarspb
+input user repo: andersen
+Select menu:
+1 - all information
+2 - contributors
+3 - followers
+4 - pull requests
+```
---
+++ task3/pull_request.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo -n "Input user name: "
+read NAME
+echo "----------------------"
+echo -n "Input user repo: "
+read REPO
+echo "----------------------"
+echo -e "Select: \n1 - all information \n2 - contributors \n3 - followers  \n4 - pull requests \n----------------------"
+read MENU 
+echo
+if [[ -z $NAME || -z $REPO || -z $MENU ]]
+then
+echo "Error! No input Name or Repo or Punkt Menu"
+elif [[ $MENU -eq 1 ]]
+then
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$NAME/$REPO/pulls
+echo "1"
+elif [[ $MENU -eq 2 ]]
+then
+echo "2"
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$NAME/$REPO/contributors | jq -r '.[] | {"Login" : .login, "id" : .id, "url" : .url}'
+elif [[ $MENU -eq 3 ]]
+then
+echo "3"
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/$NAME/followers | jq -r '.[] | {"Login" : .login, "id" : .id, "html_url" : .html_url}'
+elif [[ $MENU -eq 4 ]]
+then
+echo "4"
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$NAME/$REPO | jq -r '. | {"open pull request" : .open_issues}'
+else
+echo "Wrong menu item"
+fi
