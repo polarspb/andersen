7e1e3f7d7d1820bbd90cea567761c35408d4be66
polarspb <polarspb@debian-main.domain>
2021-03-16 05:58:02 -0400

    upload task1/one-liner.sh

---
+++ task1/one-liner.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+LINE=-----------------------------------------------
+
+echo
+echo "Input process name or PID:"
+
+read STR
+
+PINFO=$(netstat -tunapl | awk '{print "$STR" $5 $7}' | cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+')
+PNAME=$(ss -tup | sed -n '/'\"$STR'/p')
+PID=$(ss -tup | sed -n '/'=$STR'/p')
+
+echo "$LINE"
+echo
+if [ -z "$STR" ]
+then
+ echo "Error. Please input process name/ID."
+elif [[ -z "$PNAME" && -z "$PID" ]]
+then
+ echo "Error. Wrong process name/ID."
+elif [ -n "$PINFO" ] 
+then
+ echo "$PINFO" | while read IP ; do whois $IP | awk -F':' '/^Organization/ || /^Address/ || /^City/ || /^Country/ {print $0}' ; done 
+ echo
+ echo "$LINE"
+ echo
+ echo "$PINFO" | while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done | sort | uniq -c | while read COUNT ; do echo "Connections: $COUNT" ; done
+else 
+ echo "Error. No connection!"
+ echo
+fi
+echo
+echo "$LINE"
+
